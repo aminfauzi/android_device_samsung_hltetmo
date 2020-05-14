387e499c7406543c80a7008dfdce74d5e8b9b4d4
aminfauzi <aremean0107@gmail.com>
2020-05-14 22:12:26 +0000

    hltetmo: AICP-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hltetmo.mk
+    $(LOCAL_DIR)/aicp_hltetmo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_hltetmo-user \
-    lineage_hltetmo-userdebug \
-    lineage_hltetmo-eng
+    aicp_hltetmo-user \
+    aicp_hltetmo-userdebug \
+    aicp_hltetmo-eng
