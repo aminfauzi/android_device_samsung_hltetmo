e092326446c2d3a2ab50ae5c4dae0b0710500e20
aminfauzi <aremean0107@gmail.com>
2020-09-19 09:43:26 +0000

    hltetmo: Go Ressurection
    Signed-off-by: aminfauzi <aremean0107@gmail.com>
    Change-Id: Ie1d1cf9742feca328cfdc3e5c292cf38a351768a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hltetmo.mk
+    $(LOCAL_DIR)/rr_hltetmo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_hltetmo-user \
-    lineage_hltetmo-userdebug \
-    lineage_hltetmo-eng
+    rr_hltetmo-user \
+    rr_hltetmo-userdebug \
+    rr_hltetmo-eng
